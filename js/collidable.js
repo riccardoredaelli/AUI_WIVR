ecc2172b12dcac6ba9e5e340533aa4aab6418bbb
Andrea Pasquali <andreapasquali@MacBook-Pro-di-Andrea.local>
2017-11-23 14:27:57 +0100

    interazioni varie

--- js/collidable.js
+++ js/collidable.js
@@ -2,7 +2,8 @@ AFRAME.registerComponent('collider-check', {
   dependencies: ['raycaster'],
   init: function () {
     this.el.addEventListener('raycaster-intersected', function () {
-      console.log('Player hit something!' + this.el);
-    });  
+      console.log('Player hit something!');
+    });
+      
   }
 });
